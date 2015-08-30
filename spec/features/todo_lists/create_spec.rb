d278da5cc329a20f346be283987e29136f4f4b28
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-30 11:43:59 -0500

    todo description length validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -33,4 +33,40 @@ describe "Creating todo lists" do
         expect(page).to_not have_content "This is what I'm doing today."
     end
 
+    it 'displays an error when no description is input' do
+        expect(TodoList.count).to eq 0
+
+        visit '/todo_lists'
+        click_link 'New Todo list'
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "My Todo List"
+        fill_in "Description", with: ""
+        click_button "Create Todo list"
+
+        expect(page).to have_content "error"
+        expect(TodoList.count).to eq 0
+
+        visit '/todo_lists'
+        expect(page).to_not have_content "This is what I'm doing today."
+    end
+
+    it 'displays an error when a short description is input' do
+        expect(TodoList.count).to eq 0
+
+        visit '/todo_lists'
+        click_link 'New Todo list'
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "My Todo List"
+        fill_in "Description", with: "Shor"
+        click_button "Create Todo list"
+
+        expect(page).to have_content "error"
+        expect(TodoList.count).to eq 0
+
+        visit '/todo_lists'
+        expect(page).to_not have_content "This is what I'm doing today."
+    end
+
 end
